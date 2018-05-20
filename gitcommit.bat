663495b558ce8aab12261fdd6c4c2f87d234472c
gabor horvath <hgabor47@gmail.com>
2018-05-20 09:41:25 +0200

    Mouse move problem solved (I forget some and I spend 1 hour to refrech my mind :) )

--- gitcommit.bat
+++ gitcommit.bat
@@ -5,17 +5,13 @@ git add .
 git commit -m "%message%"
 git push -u origin master
 
-set p0=".\vrpd_windows"
-echo ----------%p0%
-git -C %p0% add .
-git -C %p0% commit -m "%message%"
-git -C %p0% push -u origin master
-
 set p0=".\vrpd_android"
 echo ----------%p0%
 git -C %p0% add .
 git -C %p0% commit -m "%message%"
 git -C %p0% push -u origin master
 
+cd .\vrpd_windows
+call gitcommit.bat %message%
 
 echo Finished
