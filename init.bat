1c1e0b79f9b2f72e69d78a7ef75680110e964fc6
gabor horvath <hgabor47@gmail.com>
2020-05-12 21:49:28 +0200

    Init script modified

--- init.bat
+++ init.bat
@@ -3,6 +3,6 @@ echo Clone all necessery BabylonMS modules?
 echo If this is an original clone from vrpd_windows then you need to load content to the empty modules' directories. If the modules' directories are not empty then you will get errors. So please start once only.
 echo If you would like to download contents (clone) then press a key or CTRL+C. 
 pause
-git clone https://gitlab.com/vrprelimutensdesktop/vrpd_android.git
-git clone https://gitlab.com/vrprelimutensdesktop/vrpd_windows.git
+git clone https://github.com/hgabor47/vrpd_android.git
+git clone https://github.com/hgabor47/vrpd_windows.git
 
